681ca1e27d1c102aa68f144155a401231ab3fd0b
leonardojribeiro <leonardojr410@gmail.com>
2022-06-24 19:07:53 -0300

    docs: create documentation for this api

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import { connectDatabase } from './database/connection';
 import { AppError } from './shared/errors/AppError';
 import { router } from './routes';
 import { setupDependencies } from './shared/container';
+import swagger from 'swagger-ui-express';
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
@@ -19,6 +21,8 @@ connectDatabase();
 setupDependencies();
 
 app.use(router);
+
+app.use("/", swagger.serve, swagger.setup(swaggerFile));
 
 app.use(
   (err: Error, request: express.Request, response: express.Response, _next: express.NextFunction) => {
